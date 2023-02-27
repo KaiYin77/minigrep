df4e4b9ccaf3b964dcfad0c491ee6d98737e6361
洪愷尹 <kevinhong@inquartik.com>
2023-02-27 17:23:31 +0800

    main: refactor from vector to iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 #[cfg(test)]
 mod tests {
@@ -19,7 +19,7 @@ ick three.";
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
-        let contents ="\
+        let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
@@ -39,39 +39,26 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     } else {
         search(&config.query, &contents)
     };
-    
+
     for line in results {
         println!("{line}");
     }
     Ok(())
 }
 
-pub fn search_case_insensitive<'a>(
-    query: &str, 
-    contents: &'a str
-) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 #[derive(Debug)]
@@ -82,20 +69,27 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("引數不夠");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("無法取得搜尋字串"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("無法取得檔案路徑"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { 
-            query, 
+        Ok(Config {
+            query,
             file_path,
             ignore_case,
         })
     }
 }
-
